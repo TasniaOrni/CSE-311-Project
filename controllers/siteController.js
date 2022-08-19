ef5778dbee8716fb210ef1ed44a59b3cd63baf45
Samiur Prapon <samiur.prapon@gmail.com>
2022-08-19 20:20:03 +0600

    Add contact page

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -10,8 +10,13 @@ const signup = (req, res) => {
 	res.render('signup');
 };
 
+const contact = (req, res) => {
+	res.render('contact');
+};
+
 module.exports = {
 	landing,
 	login,
 	signup,
+	contact,
 };
--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -5,5 +5,6 @@ const siteController = require('../controllers/siteController');
 router.get('/', siteController.landing);
 router.get('/login', siteController.login);
 router.get('/signup', siteController.signup);
+router.get('/contact', siteController.contact);
 
 module.exports = router;
